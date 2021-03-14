e93ba6361fa6fddde3e26b72c826cd62258ac5e8
naseef12356 <80581006+naseef12356@users.noreply.github.com>
2021-03-14 10:53:55 +0530

    Update script.py

--- script.py
+++ script.py
@@ -3,67 +3,74 @@ class script(object):
 
     START_MSG = """ <b>Hi {}
 
-You can call this as an Auto Filter Bot if you like :D
-
-This is Version 2 of Auto Filter Bot
-
-Bot gives button link to files in connected channels on query !
-
-No need to add filters for your files or movies from now on!
-
-For more click <i>help</i></b>"""
+I AM FULLY DEDICATED TO @CFCHART</b>"""
 
 
-    HELP_MSG = """<b>How to use the bot??</b>
+    HELP_MSG = """<b>MY CHANNEL LIST</b>
 
-<i>
-* Add bot to your group with admin rights.
+OLD MOVIES 
+https://t.me/joinchat/7hP8iQnONVdlY2M9
 
-* Add bot to channels which you want to link with <b>all admin rights</b>!
-</i>
+TELUGU MOVIES 
+https://t.me/joinchat/U4Gw_XLpQ_SLO6i9
+
+ALL MOVIES
+https://t.me/joinchat/R2NzDOyi85wb4EEp
+
+MALAYALAM MOVIES
+https://t.me/joinchat/QSPu1QHGNIQ2MmQ9
+
+SERIES AND FILMOGRAPHY 
+https://t.me/joinchat/R3ocwmbOOA3ASKYJ
+
+DVD UPDATES 
+https://t.me/joinchat/VUMDWO9PFmjruguU
+
+MALAYALAM SUBTITLES 
+https://t.me/joinchat/VEPyuGL5nV4ZV10G
+
+COPYRIGHTED MOVIES
+https://t.me/joinchat/UfGEgtRQzZrXUi8Z
+
+<b>Bot CHANNEL- Works in @CFCHART only</b>
+OLD MOVIES 
+https://t.me/joinchat/7hP8iQnONVdlY2M9
+
+TELUGU MOVIES 
+https://t.me/joinchat/U4Gw_XLpQ_SLO6i9
+
+ALL MOVIES
+https://t.me/joinchat/R2NzDOyi85wb4EEp
+
+MALAYALAM MOVIES
+https://t.me/joinchat/QSPu1QHGNIQ2MmQ9
+
+SERIES AND FILMOGRAPHY 
+https://t.me/joinchat/R3ocwmbOOA3ASKYJ
+
+DVD UPDATES 
+https://t.me/joinchat/VUMDWO9PFmjruguU
+
+MALAYALAM SUBTITLES 
+https://t.me/joinchat/VEPyuGL5nV4ZV10G
+
+COPYRIGHTED MOVIES
+https://t.me/joinchat/UfGEgtRQzZrXUi8Z
 
 
-<b>Bot Commands - Works in Group only</b>
-(You need to be a Auth User in order to use these commands)
+JOIN ALL CHANNEL TO GET WONDERFUL EXPERIENCE 
 
-* <code>/add channelid</code>  -  Links channel to your group.
-or
-* <code>/add @channelusername</code> - Links channel to your group.
-
-<i>NOTE : You can get your channel ID from @ChannelidHEXbot </i>
+<b>© @CFCHATOFFICIAL</b>"""
 
 
-* <code>/del channelid</code>  -  Delinks channel from group
-or
-* <code>/del @channelusername</code>  -  Delinks channel from group
-
-<i>NOTE : You can get connected channel details by <code>/filterstats</code> </i>
-
-
-* <code>/delall</code>  -  Removes all connected channels and filters from group!
-
-<i>Note : Dont add command delete bots in group! Otherwise, delall command wont work</i>
-
-
-* <code>/filterstats</code>  -  Check connected channels and number of filters.
-
-
-
-No need add each filter again!
-Bot will automatically search for your files and give links to that!
-
-
-<b>© @TroJanzHEX</b>"""
-
-
-    ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot V2</b>
+    ABOUT_MSG = """⭕️<b>DULQUER</b>
     
-⭕️<b>Creater :</b> @TroJanzHEX
+⭕️<b>Creater :</b> @CFCHART 
 
-⭕️<b>Language :</b> <code>Python3</code>
+⭕️<b>Language :</b> <code>ENGLISH L</code>
 
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
+⭕️<b>Group:</b> <a href='https://t.me/CFCHART'>my group</a> 
 
-⭕️<b>Tutorial Video :</b> <a href='https://youtu.be/KQVYQAOsFYY'>Video Link</a> 
+⭕️<b>Channel:</b> <a href='https://t.me/Cfchatofficial'>My channel</a> 
 
 """
