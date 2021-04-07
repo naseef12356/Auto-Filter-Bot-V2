c6ad6a2f9a699dc38bb69188fd1469df682df9e5
naseef12356 <80581006+naseef12356@users.noreply.github.com>
2021-04-07 09:37:55 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,17 +1,8 @@
 class script(object):
 
 
-    START_MSG = """ <b>Hi {}
-
-You can call this as an Auto Filter Bot if you like :D
-
-This is Version 2 of Auto Filter Bot
-
-Bot gives button link to files in connected channels on query !
-
-No need to add filters for your files or movies from now on!
-
-For more click <i>help</i></b>"""
+    START_MSG = """ <b>ഹായ് {}
+ഞാൻ പൂർണമായും @CFCHART <i>help</i></b>"""
 
 
     HELP_MSG = """<b>How to use the bot??</b>
